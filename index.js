8259d8b5d98b66f6d1cd0687f9383ffc3eb293f0
mardormar <44005739+mdoradom@users.noreply.github.com>
2022-06-13 15:03:57 +0200

    mejoras y documentacion del codigo

--- index.js
+++ index.js
@@ -1,60 +1,63 @@
 var time = document.querySelector(".time");
-var playButton = document.getElementById("playButton");
+var playButton = document.querySelector("button");
 var words = document.querySelector(".words");
 var timerContainer = document.querySelector(".time");
 var scoreContainer = document.querySelector(".score");
 var points = 0;
 var seconds = 60;
 var typed;
-var word;
 var spans;
 
+// list of words that will be displayed during the game
 const list = ["CASA", "AMAPOLA", "DINOSAURIO"];
 
+// this method will reset all the game and start the game from 0 points and 60 remaining seconds
 function timer() {
 	points = 0;
-	playButton.disabled = true;
-	var timeInterval = setInterval(function(){
+	var timeInterval = setInterval(function(){ // setIntervall will execute that function every 1000 ms (1 second)
+		playButton.disabled = true;
 		seconds--;
 		time.innerHTML = seconds;
-		if (seconds == 0) {
+		if (seconds === 0) {
 			scoreContainer.innerHTML = "0";
 			alert("Time is up! you've got " + points + " points")
 			words.innerHTML = "";
 			playButton.disabled = false;
 			seconds = 60;
 			timerContainer = "60";
-			clearInterval(timeInterval)
+			clearInterval(timeInterval) // this will reset the interval, otherwise it won't stop when it ends
 		}
 	}, 1000);
 }
 
+
+// this method will pick a random word from the list and will divide it with "span" tags between every letter, 
+// so you can change the background color when the player presses one letter
 function randWord() {
-	words.innerHTML = "";
-	var random = Math.floor(Math.random() * ((list.length-1) - 0 + 1)) + 0;
+	words.innerHTML = ""; // this will remove the previous displayed word, otherwise the words will stack
+	var random = Math.floor(Math.random() * ((list.length-1) - 0 + 1)) + 0; // gets the random word
 	var wordArray = list[random].split("");
 	for (var i = 0; i < wordArray.length; i++) {
 		var span = document.createElement("span");
 		span.classList.add("span");
 		span.innerHTML = wordArray[i];
 		words.appendChild(span);
-	  }
+	}
 	spans = document.querySelectorAll(".span")
 	console.log(spans)
 }
 
+// this method will check the pressed letter and compare it with the corresponding letter of the word
+// the way it does that is by seeing if the span of the letter that is checking has a "bg" propertie, if it does, it will jump to the next letter
 function typing(e) {
 	typed = String.fromCharCode(e.which);
-
 	console.log(typed);
 
 	for (var i = 0; i < spans.length; i++) {
 		if (spans[i].innerHTML === typed) {
 			if (spans[i].classList.contains("bg")) {
 				continue;
-			} else if (
-				(spans[i].classList.contains("bg") == false && spans[i - 1] == undefined) || spans[i - 1].classList.contains("bg") != false
-			) {
+			} else if (spans[i].classList.contains("bg") === false && spans[i - 1] === undefined || spans[i - 1].classList.contains("bg") !== false) {
 				spans[i].classList.add("bg");
 				break;
 			}
@@ -62,16 +65,17 @@ function typing(e) {
 	}
 
 	var checker = 0;
-	for (var i = 0; i < spans.length; i++) {
-		if (spans[i].className == "span bg") {
+	for (var j = 0; j < spans.length; j++) {
+		if (spans[j].className === "span bg") {
 			checker++;
 		}
 
-		if (checker == spans.length) {
-			words.classList.add("animated");
+		if (checker === spans.length) {
+			words.classList.add("animate");
 			words.classList.add("fadeOut");
 			points++;
 			scoreContainer.innerHTML = points;
+			document.removeEventListener("keydown", typing, false);
 			setTimeout(function() {
 				words.className = "words"
 				randWord();
@@ -81,9 +85,12 @@ function typing(e) {
 	}
 }
 
+// this event will call the method timer() and randWord() when you press the Play button
 playButton.addEventListener("click", function(e) {
 	timer();
 	randWord();
+	playButton.disabled = true;
 });
 
+// this method will call the method typing() and send the key that was typed to the method
 document.addEventListener("keydown", typing, false);
