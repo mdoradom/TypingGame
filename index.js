403c4be4a5741f7258c70060c85be6f761e98941
mardormar <44005739+mdoradom@users.noreply.github.com>
2022-06-13 13:01:55 +0200

    funciona de momento

--- index.js
+++ index.js
@@ -1,92 +1,89 @@
-var temp = document.querySelector('.time');
-var button = document.querySelector("button");
+var time = document.querySelector(".time");
+var playButton = document.getElementById("playButton");
 var words = document.querySelector(".words");
-var timerDiv = document.querySelector(".time");
-var scoreDiv = document.querySelector(".score");
+var timerContainer = document.querySelector(".time");
+var scoreContainer = document.querySelector(".score");
 var points = 0;
-var spans;
-var typed;
 var seconds = 60;
+var typed;
+var word;
+var spans;
 
- 	function countdown() {
- 		points = 0;
- 		var timer = setInterval(function(){
- 			button.disabled = true;
-    		seconds--;
-    		temp.innerHTML = seconds;
-    		if (seconds === 0) {
-    			alert("Game over! Your score is " + points);
-    			scoreDiv.innerHTML = "0";
-    			words.innerHTML = "";
-    			button.disabled = false;
-    			clearInterval(timer);
-    			seconds = 60;
-    			timerDiv.innerHTML = "60";
-    			button.disabled = false;	
-    		}
- 		}, 1000);
-  	}
+const list = ["CASA", "AMAPOLA", "DINOSAURIO"];
 
-  	function random() {
-  		words.innerHTML = "";
-  		var random = Math.floor(Math.random() * (1943 - 0 + 1)) + 0;
-  		var wordArray = list[random].split("");
-  		for (var i = 0; i < wordArray.length; i++) { //building the words with spans around the letters
-  			var span = document.createElement("span");
-  			span.classList.add("span");
-  			span.innerHTML = wordArray[i];
-  			words.appendChild(span);
-  		}
-  		spans = document.querySelectorAll(".span");
-  	}
+function timer() {
+	points = 0;
+	playButton.disabled = true;
+	var timeInterval = setInterval(function(){
+		seconds--;
+		time.innerHTML = seconds;
+		if (seconds == 0) {
+			scoreContainer.innerHTML = "0";
+			alert("Time is up! you've got " + points + " points")
+			words.innerHTML = "";
+			playButton.disabled = false;
+			seconds = 60;
+			timerContainer = "60";
+			clearInterval(timeInterval)
+		}
+	}, 1000);
+}
 
+function randWord() {
+	words.innerHTML = "";
+	var random = Math.floor(Math.random() * ((list.length-1) - 0 + 1)) + 0;
+	var wordArray = list[random].split("");
+	for (var i = 0; i < wordArray.length; i++) {
+		var span = document.createElement("span");
+		span.classList.add("span");
+		span.innerHTML = wordArray[i];
+		words.appendChild(span);
+	  }
+	spans = document.querySelectorAll(".span")
+	console.log(spans)
+}
 
-  	const list = []
+function typing(e) {
+	typed = String.fromCharCode(e.which);
 
-    for (var i = 0; i < 100; i++) {
-        list(i) = // https://random-words-api.vercel.app/word
-        // hacer query a la api de https://github.com/mcnaveen/Random-Words-API para que devuelva una palabra aleatoria y añadirla a la lista
-        // tambien habría que pasarlas a mayusculas para que quede mejor visualmente.
-    }
+	console.log(typed);
 
-  	button.addEventListener("click", function(e){
-  		countdown();
-  		random();
-  		button.disabled = true;	
-  	});
+	for (var i = 0; i < spans.length; i++) {
+		if (spans[i].innerHTML === typed) {
+			if (spans[i].classList.contains("bg")) {
+				continue;
+			} else if (
+				(spans[i].classList.contains("bg") == false && spans[i - 1] == undefined) || spans[i - 1].classList.contains("bg") != false
+			) {
+				spans[i].classList.add("bg");
+				break;
+			}
+		}
+	}
 
+	var checker = 0;
+	for (var i = 0; i < spans.length; i++) {
+		if (spans[i].className == "span bg") {
+			checker++;
+		}
 
-  	function typing(e) {
-  			typed = String.fromCharCode(e.which);
-  			for (var i = 0; i < spans.length; i++) {
-  				if (spans[i].innerHTML === typed) { // if typed letter is the one from the word
-  					if (spans[i].classList.contains("bg")) { // if it already has class with the bacground color then check the next one
-  						continue;
-  					} else if (spans[i].classList.contains("bg") === false && spans[i-1] === undefined || spans[i-1].classList.contains("bg") !== false ) { // if it dont have class, if it is not first letter or if the letter before it dont have class (this is done to avoid marking the letters who are not in order for being checked, for example if you have two "A"s so to avoid marking both of them if the first one is at the index 0 and second at index 5 for example)
-  						spans[i].classList.add("bg");
-  						break;
-  					}
-  				}
-  			}
-  			var checker = 0;
-  			for (var j = 0; j < spans.length; j++) { //checking if all the letters are typed
-  				if (spans[j].className === "span bg") {
-  					checker++;
-  				}
-  				if (checker === spans.length) { // if so, animate the words with animate.css class
-  					words.classList.add("animated");
-  					words.classList.add("fadeOut");
-  					points++; // increment the points
-  					scoreDiv.innerHTML = points; //add points to the points div
-  					document.removeEventListener("keydown", typing, false);
-  					setTimeout(function(){
-  						words.className = "words"; // restart the classes
-  						random(); // give another word
-  						document.addEventListener("keydown", typing, false);
-  					}, 400);
-  				}
+		if (checker == spans.length) {
+			words.classList.add("animated");
+			words.classList.add("fadeOut");
+			points++;
+			scoreContainer.innerHTML = points;
+			setTimeout(function() {
+				words.className = "words"
+				randWord();
+				document.addEventListener("keydown", typing, false);
+			}, 400);
+		}
+	}
+}
 
-  			}
-  	}
+playButton.addEventListener("click", function(e) {
+	timer();
+	randWord();
+});
 
-  	document.addEventListener("keydown", typing, false);
+document.addEventListener("keydown", typing, false);
